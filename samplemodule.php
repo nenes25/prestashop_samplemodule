519bdd43d2d9b2b8c000757a96ea62ea90629367
Hervé Hennes <hhennes@advisa.fr>
2018-11-07 13:50:11 +0100

    WIP admin

--- samplemodule.php
+++ samplemodule.php
@@ -1,11 +1,14 @@
 <?php
+
 /**
  * Sample Prestashop Module
+ *
+ * Module de démonstration des différents principes prestashop
  *
  * @author Hhennes
  *
  */
-class samplemodule extends Module
+class SampleModule extends Module
 {
 
     public function __construct()
@@ -13,7 +16,7 @@ class samplemodule extends Module
         $this->author = 'hhennes';
         $this->name = 'samplemodule';
         $this->tab = 'hhennes';
-        $this->version = '0.1.0';
+        $this->version = '0.1.1';
         $this->need_instance = 0;
 
         parent::__construct();
@@ -24,18 +27,82 @@ class samplemodule extends Module
 
     /**
      * Installation du module
+     * @return boolean
      */
     public function install()
     {
-        return parent::install();
+        return parent::install() && $this->_installSql() && $this->_installTab();
     }
 
     /**
-     * Désinstalation du module
+     * Désinstallation du module
+     * @return boolean
      */
     public function uninstall()
     {
-        return parent::uninstall();
+        return parent::uninstall() && $this->_uninstallSql() && $this->_uninstallTab();
+    }
+
+    /**
+     * Création de la base de donnée
+     * @return boolean
+     */
+    protected function _installSql()
+    {
+
+    }
+
+    /**
+     * Installation du controller dans la backoffice
+     * @return boolean
+     */
+    protected function _installTab()
+    {
+        $tab = new Tab();
+        $tab->class_name = 'AdminHhSample';
+        $tab->module = $this->name;
+        $tab->id_parent = (int)Tab::getIdFromClassName('DEFAULT');
+        $tab->icon = 'settings_applications';
+        $languages = Language::getLanguages();
+        foreach ($languages as $lang) {
+            $tab->name[$lang['id_lang']] = $this->l('HH Sample Admin controller');
+        }
+        try {
+            $tab->save();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Désinstallation du controller admin
+     * @return boolean
+     */
+    protected function _uninstallTab()
+    {
+        $idTab = (int)Tab::getIdFromClassName('AdminHhSample');
+        if ($idTab) {
+            $tab = new Tab($idTab);
+            try {
+                $tab->delete();
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Suppression de la base de données
+     */
+    protected function _uninstallSql()
+    {
+        $sql = "DROP TABLE hh_sample,hh_sample_lang";
+        return Db::getInstance()->execute($sql);
     }
 
 }
