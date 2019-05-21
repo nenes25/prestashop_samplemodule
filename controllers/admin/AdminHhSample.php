9326c2b20d3d0209799b1c791d9f01d046852858
Hervé Hennes <hhennes@advisa.fr>
2019-05-21 11:24:00 +0200

    Gestion des options

--- controllers/admin/AdminHhSample.php
+++ controllers/admin/AdminHhSample.php
@@ -73,13 +73,134 @@ class AdminHhSampleController extends ModuleAdminController
                 'icon' => 'icon-trash',
                 'confirm' => $this->l('Delete selected items?')
             ],
-            'status' =>[
+            'status' => [
                 'text' => $this->l('Delete selected'),
                 'icon' => 'icon-trash',
                 'confirm' => $this->l('Delete selected items?')
             ],
         ];
 
+        /**
+         * Gestion des options
+         * Lors de la sauvegarde des options, les clés des options sont enregistrée dans la table de configuration
+         */
+        $this->fields_options = [
+            //Groupe d'option n 1
+            'general' => [
+                //Titre du groupe d'options
+                'title' => $this->l('General configuration'),
+                //Champs du group d'options
+                'fields' => [
+                    //Exemple option de type texte
+                    'OPTION_KEY_TEXT' => [ //La clé du tableau correspond au nom de la configuration
+                        'title' => $this->l('Field Text'),
+                        'hint' => $this->l('Field Text Hint'),
+                        'validation' => 'isWeightUnit', //Classe de validation ( de la classe Validate )
+                        'required' => true, //Champ requis ou non
+                        'type' => 'text', //Type de champ
+                        'class' => 'fixed-width-sm' //classe css
+                    ],
+                    //Exemple option de type texte avec langue
+                    'OPTION_KEY_TEXT_LANG' => [
+                        'title' => $this->l('Field Text Lang'),
+                        'hint' => $this->l('Field Text Lang Hint'),
+                        'validation' => 'isWeightUnit',
+                        'required' => true,
+                        'type' => 'textLang', //Type de champ
+                        'class' => 'fixed-width-sm'
+                    ],
+                    //Exemple option de type textarea
+                    'OPTION_KEY_TEXTAREA' => [
+                        'title' => $this->l('Field Textarea'),
+                        'hint' => $this->l('Field Textarea Hint'),
+                        'required' => true,
+                        'type' => 'textarea',
+                        'cols' => 150, //Champ requis Textarea
+                        'rows' => 2 // Champ requis Textarea
+                    ],
+                    //Exemple option de type textarea lang
+                    'OPTION_KEY_TEXTAREA_LANG' => [
+                        'title' => $this->l('Field Textarea Lang'),
+                        'hint' => $this->l('Field Textarea Hint'),
+                        'required' => true,
+                        'type' => 'textareaLang',
+                        'cols' => 150, //Champ requis Textarea
+                        'rows' => 2 // Champ requis Textarea
+                    ],
+                    //Exemple option de type textarea avec TinyMce
+                    'OPTION_KEY_TEXTAREA_MCE' => [
+                        'title' => $this->l('Field Textarea editor'),
+                        'hint' => $this->l('Field Textarea editor Hint'),
+                        'required' => true,
+                        'type' => 'textarea',
+                        'autoload_rte' => true ,//Flag pour afficher l'éditeur de texte
+                        'cols' => 150, //Champ requis Textarea
+                        'rows' => 2 // Champ requis Textarea
+                    ],
+                    //Exemple d'option de type "select"
+                    'OPTION_KEY_SELECT' => [
+                        'title' => $this->l('Sample select fields'),
+                        'hint' => $this->l('This is a select field.'),
+                        'cast' => 'intval', //Cast appliqué aux valeurs
+                        'type' => 'select',
+                        'identifier' => 'id_lang', //Champs requis select clé de l'identifiant
+                        'list' => Language::getLanguages(false) //Champ requis select source des données
+                    ],
+                    //Exemple d'option de type boolean
+                    'OPTION_KEY_BOOLEAN' => [
+                        'title' => $this->l('Bolean Field'),
+                        'hint' => $this->l('Bolean Field Hint'),
+                        'validation' => 'isBool',
+                        'cast' => 'intval',
+                        'type' => 'bool',
+                        'default' => '1' //Valeur défaut pour l'option boolean
+                    ],
+                    //Exemple d'option de type file
+                    'OPTION_KEY_FILE' => [
+                        'title' => $this->l('File Field'),
+                        'hint' => $this->l('File Field Hint'),
+                        'type' => 'file',
+                        'name' => 'OPTION_KEY_FILE' //Champ requis file
+                    ],
+                    //Exemple d'option de type file avec un thumb
+                    'OPTION_KEY_THUMB' => [
+                        'title' => $this->l('Thumb Field'),
+                        'hint' => $this->l('Thumb Field Hint'),
+                        'type' => 'file',
+                        'name' => 'OPTION_KEY_FILE',
+                        'thumb' => 'https://upload.wikimedia.org/wikipedia/commons/e/ee/Thumbup.jpg' //Champ requis thumb
+                    ],
+                    //Exemple d'option de type color
+                    'OPTION_KEY_COLOR' => [
+                        'title' => $this->l('Color Field'),
+                        'hint' => $this->l('Color Field Hint'),
+                        'validation' => 'isColor',
+                        'type' => 'color',
+                        'name' => 'OPTION_KEY_COLOR', // Champs requis Color
+                        'size' => 7, //CHamps requis Color
+                    ],
+
+                ],
+                //Bouton de soumission
+                'submit' => ['title' => $this->l('Save')]
+            ],
+            //Groupe d'option n'2
+            'localization' => [
+                'title' => $this->l('Option bloc 2'),
+                'icon' => 'icon-globe',
+                'fields' => [
+                    'OPTION_KEY_TEXT_2' => [
+                        'title' => $this->l('Field Text'),
+                        'hint' => $this->l('Field Text Hint'),
+                        'validation' => 'isWeightUnit',
+                        'required' => true,
+                        'type' => 'text',
+                        'class' => 'fixed-width-sm'
+                    ],
+                ],
+                'submit' => ['title' => $this->l('Save')]
+            ],
+        ];
     }
 
     /**
@@ -168,7 +289,7 @@ class AdminHhSampleController extends ModuleAdminController
         //Ajout d'un bouton qui appelle une fonction du controller
         //La fonction appellée sera process{Action} => donc ProcessCustom
         $this->page_header_toolbar_btn['custom_button'] = array(
-            'href' => self::$currentIndex . '&token=' . $this->token.'&action=custom',
+            'href' => self::$currentIndex . '&token=' . $this->token . '&action=custom',
             'desc' => $this->module->l('Custom button'),
             'icon' => 'process-icon-configure'
         );
@@ -194,7 +315,7 @@ class AdminHhSampleController extends ModuleAdminController
             case 'edit':
                 array_pop($this->toolbar_title);
                 if (($sample = $this->loadObject(true)) && Validate::isLoadedObject($sample)) {
-                    $this->toolbar_title[] = sprintf($this->module->l('Editing sample %s'),$sample->name);
+                    $this->toolbar_title[] = sprintf($this->module->l('Editing sample %s'), $sample->name);
                 } else {
                     $this->toolbar_title[] = $this->module->l('Creating a new sample');
                 }
@@ -205,7 +326,8 @@ class AdminHhSampleController extends ModuleAdminController
     /**
      * Action spécifique pour le controller
      */
-    public function processCustom(){
-       dump('custom Action called by the button toolbar');
+    public function processCustom()
+    {
+        dump('custom Action called by the button toolbar');
     }
 }
